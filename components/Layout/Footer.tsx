7a74e0133eb90419a57cd9d475373dd65a501d24
CarnkyGus <jirateep.loyma02@gmail.com>
2022-08-03 18:46:59 +0700

    Update: Footer

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -3,8 +3,14 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <div className="bg-white text-black font-[MinimalRecular] space-x-1 h-12 flex  justify-center items-center ">
-      <h3>
-        © 2022 by <span className="font-[MinimalBold]">AMYOL</span> DEV.
+      <h3 className="cursor-default">
+        © 2022 by{" "}
+        <span className="font-[MinimalBold]">
+          <Link href={"/"}>
+            <a>AMYOL</a>
+          </Link>
+        </span>{" "}
+        DEV.
       </h3>
       <div className="px-6 space-x-4 hidden md:flex">
         <p>|</p>
